46cd29f8740ce2f0cfc05adb957d6c79890618fd
lased <61lased61@gmail.com>
2021-12-02 15:14:48 +0300

    Исправление ошибок

--- app/Views/Panel/Article/ListView.php
+++ app/Views/Panel/Article/ListView.php
@@ -44,82 +44,86 @@ $queryString = $this->route['queryString'];
     </ul>
   </div>
 </div>
-<table class="table content-table">
-  <thead class="table__thead">
-    <tr class="table__thead-tr">
-      <th class="table__thead-th content-table__th">Дата публикации</th>
-      <th class="table__thead-th content-table__th">Заголовок</th>
-      <th class="table__thead-th content-table__th">Действия</th>
-    </tr>
-  </thead>
-  <tfoot class="table__tfoot">
-    <tr>
-      <td class="table__tfoot-td" colspan="3">
-        <div class="content-pagination"><span>Записей на странице:</span>
-          <div class="select content-pagination__select">
-            <ul class="select__current" tabindex="0">
-              <? foreach ($rowlimits as $value => $checked) : ?>
-                <li class="select__value">
-                  <input class="select__input" id="pagination-select-value-<?= $value ?>" type="radio" name="limit" value="<?= $value ?>" <?= $checked ? 'checked' : '' ?>>
-                  <div class="select__text"><?= $value ?></div>
-                </li>
-              <? endforeach; ?>
-              <i class="select__arrow fa fa-angle-down"></i>
-            </ul>
-            <ul class="select__list">
-              <? foreach ($rowlimits as $value => $checked) : ?>
-                <li><label class="select__label" for="pagination-select-value-<?= $value ?>"><?= $value ?></label></li>
-              <? endforeach; ?>
-            </ul>
-          </div>
-          <span>
-            <? $start = ($page - 1) * $limit + 1; ?>
-            <?= count($articles) === 0 ? '0-0' : $start . '-' . ($start - 1 + count($articles)) ?>
-            из <?= $rowsCount ?>
-          </span>
-          <div class="content-pagination__arrows">
-            <?
-            $pageString = $queryString->get('page');
-            $queryString->set('page', $page - 1);
-            ?>
-            <button class="btn content-pagination__btn" <? if (!($page - 1)) : ?> disabled <? endif; ?>>
-              <a class="content-pagination__link" href="<?= '?' . $queryString->toString() ?>">
-                <i class="fa fa-arrow-left"></i>
-              </a>
-            </button>
-            <? $queryString->set('page', $page + 1); ?>
-            <button class="btn content-pagination__btn" <? if (ceil($rowsCount / $limit) == $page) : ?> disabled <? endif; ?>>
-              <a class="content-pagination__link" href="<?= '?' . $queryString->toString() ?>">
-                <i class="fa fa-arrow-right"></i>
-              </a>
-            </button>
-            <? $pageString ?? $queryString->remove('page'); ?>
-          </div>
-        </div>
-      </td>
-    </tr>
-  </tfoot>
-  <tbody class="table__tbody">
-    <? foreach ($articles as $article) : ?>
-      <tr class="table__tbody-tr">
-        <td class="table__tbody-td content-table__tbody-td"><?= $article['created_at'] ?></td>
-        <td class="table__tbody-td content-table__tbody-td">
-          <a class="table__link content-table__link" href="/panel/article/update/<?= $article['id'] ?>">
-            <img class="content-table__image" src="<?= $article['image'] ?>">
-            <span class="content-table__title"><?= $article['title'] ?></span>
-          </a>
-        </td>
-        <td class="table__tbody-td content-table__tbody-td">
-          <div class="content-table__group-btn">
-            <a class="btn btn-secondary content-table__action" href="/panel/article/update/<?= $article['id'] ?>">
-              <i class="fa fa-pen"></i>
-            </a>
-            <a class="btn btn-warning content-table__action" href="/panel/article/delete/<?= $article['id'] ?>">
-              <i class="fa fa-trash"></i>
-            </a>
+<? if (count($articles)) : ?>
+  <table class="table content-table">
+    <thead class="table__thead">
+      <tr class="table__thead-tr">
+        <th class="table__thead-th content-table__th">Дата публикации</th>
+        <th class="table__thead-th content-table__th">Заголовок</th>
+        <th class="table__thead-th content-table__th">Действия</th>
+      </tr>
+    </thead>
+    <tfoot class="table__tfoot">
+      <tr>
+        <td class="table__tfoot-td" colspan="3">
+          <div class="content-pagination"><span>Записей на странице:</span>
+            <div class="select content-pagination__select">
+              <ul class="select__current" tabindex="0">
+                <? foreach ($rowlimits as $value => $checked) : ?>
+                  <li class="select__value">
+                    <input class="select__input" id="pagination-select-value-<?= $value ?>" type="radio" name="limit" value="<?= $value ?>" <?= $checked ? 'checked' : '' ?>>
+                    <div class="select__text"><?= $value ?></div>
+                  </li>
+                <? endforeach; ?>
+                <i class="select__arrow fa fa-angle-down"></i>
+              </ul>
+              <ul class="select__list">
+                <? foreach ($rowlimits as $value => $checked) : ?>
+                  <li><label class="select__label" for="pagination-select-value-<?= $value ?>"><?= $value ?></label></li>
+                <? endforeach; ?>
+              </ul>
+            </div>
+            <span>
+              <? $start = ($page - 1) * $limit + 1; ?>
+              <?= count($articles) === 0 ? '0-0' : $start . '-' . ($start - 1 + count($articles)) ?>
+              из <?= $rowsCount ?>
+            </span>
+            <div class="content-pagination__arrows">
+              <?
+              $pageString = $queryString->get('page');
+              $queryString->set('page', $page - 1);
+              ?>
+              <button class="btn content-pagination__btn" <? if (!($page - 1)) : ?> disabled <? endif; ?>>
+                <a class="content-pagination__link" href="<?= '?' . $queryString->toString() ?>">
+                  <i class="fa fa-arrow-left"></i>
+                </a>
+              </button>
+              <? $queryString->set('page', $page + 1); ?>
+              <button class="btn content-pagination__btn" <? if (ceil($rowsCount / $limit) == $page) : ?> disabled <? endif; ?>>
+                <a class="content-pagination__link" href="<?= '?' . $queryString->toString() ?>">
+                  <i class="fa fa-arrow-right"></i>
+                </a>
+              </button>
+              <? $pageString ?? $queryString->remove('page'); ?>
+            </div>
           </div>
         </td>
       </tr>
-    <? endforeach; ?>
-  </tbody>
-</table>
+    </tfoot>
+    <tbody class="table__tbody">
+      <? foreach ($articles as $article) : ?>
+        <tr class="table__tbody-tr">
+          <td class="table__tbody-td content-table__tbody-td"><?= $article['created_at'] ?></td>
+          <td class="table__tbody-td content-table__tbody-td">
+            <a class="table__link content-table__link" href="/panel/article/update/<?= $article['id'] ?>">
+              <img class="content-table__image" src="<?= $article['image'] ?>">
+              <span class="content-table__title"><?= $article['title'] ?></span>
+            </a>
+          </td>
+          <td class="table__tbody-td content-table__tbody-td">
+            <div class="content-table__group-btn">
+              <a class="btn btn-secondary content-table__action" href="/panel/article/update/<?= $article['id'] ?>">
+                <i class="fa fa-pen"></i>
+              </a>
+              <a class="btn btn-warning content-table__action" href="/panel/article/delete/<?= $article['id'] ?>">
+                <i class="fa fa-trash"></i>
+              </a>
+            </div>
+          </td>
+        </tr>
+      <? endforeach; ?>
+    </tbody>
+  </table>
+<? else : ?>
+  <h2 style="text-align: center;">Записей не найдено</h2>
+<? endif; ?>
